75b25c6cd5a2d849326263c049e39ba448e3425d
Auanassov Chingiz <auanassov.chingiz@gmail.com>
2014-02-21 10:17:28 +0600

    split recipes that work with db

---
+++ recipes/_simple_deploy_with_migrations_and_db_seed.rb
@@ -0,0 +1,4 @@
+include_recipe "robux::update_source"
+include_recipe "robux::compile_source"
+include_recipe "robux::db_migrate"
+include_recipe "robux::db_seed"
--- recipes/db_seed_bots.rb
+++ recipes/db_seed_bots.rb
@@ -4,9 +4,6 @@ bash "run migrations, seed database and seed demo data" do
   group node.group
   code <<-EOC
     cd #{node.robux.dirs.base_dir}/#{node.robux.dirs.app}
-    #{node.rvm_bundle} exec rake db:migrate RAILS_ENV=#{node.robux.rails_env}
+    #{node.rvm_bundle} exec rake db:seed RAILS_ENV=#{node.robux.rails_env}
   EOC
 end
-
-include_recipe "robux::db_seed"
-include_recipe "robux::db_demo_data_seed"
